bb9109267430b923e2dfa445a066e775fdb175b6
wooseokHan <hosmimam@naver.com>
2025-02-06 15:10:51 +0900

    feat : add style to main page

---
+++ src/app/page.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+import { MOBILE_MAX_WIDTH } from "@/shared/consts";
+
+export const container = style({
+  maxWidth: MOBILE_MAX_WIDTH,
+  margin: "0 auto",
+  minHeight: "100vh",
+  backgroundColor: "#000",
+});
