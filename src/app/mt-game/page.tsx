92737d04cce51a9d0e3505adb5f71d0423b12643
wooseokHan <hosmimam@naver.com>
2025-02-19 02:38:19 +0900

    Feat : add music data

--- src/app/mt-game/page.tsx
+++ src/app/mt-game/page.tsx
@@ -1,14 +1,16 @@
-// import apolloClient from "@/shared/lib/apollo";
-// import { GET_ALL_GENRES } from "./page.query";
+import apolloClient from "@/shared/lib/apollo";
+import { GET_ALL_GENRES } from "./page.query";
 
 export default async function MTGame() {
-  // const { data } = await apolloClient.query({
-  //   query: GET_ALL_GENRES,
-  //   fetchPolicy: "cache-first",
-  //   context: {
-  //     caches: "force-cache",
-  //   },
-  // });
+  const { data } = await apolloClient.query({
+    query: GET_ALL_GENRES,
+    fetchPolicy: "cache-first",
+    context: {
+      caches: "force-cache",
+    },
+  });
+
+  console.log(data);
 
   return <main></main>;
 }
